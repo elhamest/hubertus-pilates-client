db23b16b9cbbd0a30a747a24854e9d51ca81e2d7
elhamest <e.estedlali@gmail.com>
2024-11-22 22:15:58 +0330

    add export to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Ensure export mode
+  images: {
+    unoptimized: true, // Github Pages doesn't support Next.js optimization for images
+  },
+};
 
 export default nextConfig;
